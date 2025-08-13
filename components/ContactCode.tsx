ab044f93ec9cea0d81dcf9ddc27226f692ce97cc
Shikhar Pandey <pandeyshikhar07@gmail.com>
2025-08-14 04:36:45 +0530

    ContactCode.tsx

--- components/ContactCode.tsx
+++ components/ContactCode.tsx
@@ -1,13 +1,13 @@
 import styles from '@/styles/ContactCode.module.css';
 
 const contactItems = [
-  { social: 'website', link: 'shikhars-portfolio.vercel.app', href: 'https://shikhars-portfolio.vercel.app/' },
+  { social: 'website', link: 'shikhars-portfolio.vercel.app', href: 'https://shikharportfolio.vercel.app/' },
   { social: 'email', link: 'pandeyshikhar07@gmail.com', href: 'mailto:pandeyshikhar07@gmail.com' },
   { social: 'github', link: 'pandeyshikhar18', href: 'https://github.com/pandeyshikhar18' },
   { social: 'linkedin', link: 'shikharpandey', href: 'https://www.linkedin.com/in/shikhar-pandey-a54442222/' },
   { social: 'twitter', link: 'shikharp18', href: 'https://www.twitter.com/shikharp18' },
   { social: 'instagram', link: 'shikharp18', href: 'https://www.instagram.com/shikharp18' },
-  { social: 'resume', link: 'View Resume', href: 'https://drive.google.com/file/d/1TZvo6uZt0IsQkiUsuCMFYoeYSQjxzeu5/view?usp=sharing' }, // added
+  { social: 'resume', link: 'View Resume', href: 'https://drive.google.com/file/d/1TZvo6uZt0IsQkiUsuCMFYoeYSQjxzeu5/view?usp=sharing' }, 
 ];
 
 const ContactCode = () => {
